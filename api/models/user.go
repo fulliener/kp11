dfcd97f1e206b1d9b52b7a50050f68820aebbd73
Ruslan Akperov <125014306+fulliener@users.noreply.github.com>
2024-12-17 06:55:14 +0300

    Delete api directory

--- api/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID    int    `db:"id" json:"ID"`
-	Image string `db:"image" json:"Image"`
-	Name  string `db:"name" json:"Name"`
-	Phone string `db:"phone" json:"Phone"`
-	Mail  string `db:"mail" json:"Mail"`
-}
